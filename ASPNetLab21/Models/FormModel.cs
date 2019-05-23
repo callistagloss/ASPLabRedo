24e682a0ac5592cc803169f68688daa60575d5df
Callista <theevenstarlight@gmail.com>
2019-05-23 15:39:35 -0400

    first commit

---
+++ ASPNetLab21/Models/FormModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNetLab21.Controllers
+{
+    public class FormModel
+    {
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public string Chewie { get; set; }
+        public string HanSolo {get; set;}
+        public string 
+
+        public string Radio { get; set; }
+        
+        public string Weapon { get; set; }
+    }
+}
